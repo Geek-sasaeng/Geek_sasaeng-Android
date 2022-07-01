90ff88610aa6d1b9ff1abb63f4763c03af4e3d4f
lee-june-young <uhiccup@gmail.com>
2022-07-01 22:34:59 +0900

    Style: CreatePartyFragment layout

---
+++ app/src/main/java/com/example/geeksasaeng/CreatePartyFragment.kt
@@ -0,0 +1,19 @@
+package com.example.geeksasaeng
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.geeksasaeng.Base.BaseFragment
+import com.example.geeksasaeng.databinding.FragmentCreatePartyBinding
+import com.example.geeksasaeng.databinding.FragmentStepOneBinding
+
+
+class CreatePartyFragment : BaseFragment<FragmentCreatePartyBinding>(FragmentCreatePartyBinding::inflate) {
+
+    override fun initAfterBinding() {
+
+    }
+
+}
