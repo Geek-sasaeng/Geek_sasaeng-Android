b115a9e0a0e5cfb41c245b3cc957299a3e1276d1
lee-june-young <uhiccup@gmail.com>
2022-07-01 20:08:29 +0900

    Feat:SignUpActivity에 뷰페이저 적용

--- app/src/main/java/com/example/geeksasaeng/SignUpActivity.kt
+++ app/src/main/java/com/example/geeksasaeng/SignUpActivity.kt
@@ -10,8 +10,26 @@ class SignUpActivity : BaseActivity<ActivitySignUpBinding>(ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.signUpNextBtn.setOnClickListener {
+        val signUpAdapter = SignUpVPAdapter(this)
+        binding.signUpVp.setUserInputEnabled(false) // 뷰페이저 스와이프 막기
+        binding.signUpVp.adapter = signUpAdapter //어댑터 연결
 
+        initClickListener()
+    }
+
+    private fun initClickListener(){
+
+        binding.signUpNextBtn.setOnClickListener { //다음버튼의 클릭이벤트 처리
+            var currentPos= binding.signUpVp.currentItem
+
+            if (currentPos==2){ //마지막 페이지
+                //회원가입 진행
+            }else{ //마지막 페이지가 아니면 다음 페이지로 넘겨주기
+                if(currentPos==1){ //이때 대마지막 전페이지면
+                    binding.signUpNextBtn.text = "시작하기"
+                }
+                binding.signUpVp.setCurrentItem(currentPos+1, false)
+            }
         }
     }
 }
