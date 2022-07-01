4ce5e2fe343b3319b10a01242ec70dff751e88fe
lee-june-young <uhiccup@gmail.com>
2022-07-01 16:59:38 +0900

    이메일 인증까지 layout

---
+++ app/src/main/java/com/example/geeksasaeng/StepThreeFragment.kt
@@ -0,0 +1,18 @@
+package com.example.geeksasaeng
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.geeksasaeng.Base.BaseFragment
+import com.example.geeksasaeng.databinding.FragmentStepThreeBinding
+
+
+class StepThreeFragment : BaseFragment<FragmentStepThreeBinding>(FragmentStepThreeBinding::inflate) {
+
+    override fun initAfterBinding() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/geeksasaeng/StepTwoFragment.kt
@@ -0,0 +1,18 @@
+package com.example.geeksasaeng
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.geeksasaeng.Base.BaseFragment
+import com.example.geeksasaeng.databinding.FragmentStepTwoBinding
+
+
+class StepTwoFragment : BaseFragment<FragmentStepTwoBinding>(FragmentStepTwoBinding::inflate) {
+
+    override fun initAfterBinding() {
+
+    }
+
+}
