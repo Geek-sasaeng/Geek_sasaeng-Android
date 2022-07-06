31e7aa167e7817b7449888b2326ac9683f6ee3a9
JM2308 <wlals2987@naver.com>
2022-07-07 01:49:55 +0900

    Refactor: Signup ProgressBar to SeekBar

--- app/src/main/java/com/example/geeksasaeng/Signup/ProgressViewModel.kt
+++ app/src/main/java/com/example/geeksasaeng/Signup/ProgressViewModel.kt
@@ -11,10 +11,6 @@ class ProgressViewModel: ViewModel() {
         currentPro.value = 0
     }
 
-    fun setPro(progress: Int) {
-        currentPro.value = currentPro.value?.plus(1)
-    }
-
     fun increase() {
         currentPro.value = currentPro.value!! + 1
         Log.d("PROGRESS-STATUS", "CURRENTPRO = " + currentPro.value.toString())
