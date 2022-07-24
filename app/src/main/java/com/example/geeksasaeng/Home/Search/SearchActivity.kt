6e6bbf3b0022b2e8d6fb014823bffea30972b418
JM2308 <wlals2987@naver.com>
2022-07-24 16:21:05 +0900

    Fix: SearchActivity Basic/Detail Fragment Stack Error

--- app/src/main/java/com/example/geeksasaeng/Home/Search/SearchActivity.kt
+++ app/src/main/java/com/example/geeksasaeng/Home/Search/SearchActivity.kt
@@ -3,6 +3,7 @@ package com.example.geeksasaeng.Home.Search
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.geeksasaeng.Home.Delivery.Adapter.DeliveryRVAdapter
 import com.example.geeksasaeng.R
@@ -20,15 +21,19 @@ class SearchActivity: BaseActivity<ActivitySearchBinding>(ActivitySearchBinding:
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) {
-                if (binding.searchEt.length() == 0)
-                    supportFragmentManager.beginTransaction().replace(R.id.search_frm, SearchBasicFragment()).commit()
+                if (binding.searchEt.length() == 0) {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.search_frm, SearchBasicFragment())
+                        .addToBackStack("SearchBasic").commit()
+                }
             }
         })
     }
 
     fun initClickListener() {
         binding.searchBtn.setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(R.id.search_frm, SearchDetailFragment()).commit()
+            supportFragmentManager.popBackStack("SearchDetail", FragmentManager.POP_BACK_STACK_INCLUSIVE)
+            supportFragmentManager.beginTransaction().replace(R.id.search_frm, SearchDetailFragment()).addToBackStack("SearchDetail").commit()
         }
     }
 }
--- app/src/main/java/com/example/geeksasaeng/Home/Search/SearchBasicFragment.kt
+++ app/src/main/java/com/example/geeksasaeng/Home/Search/SearchBasicFragment.kt
@@ -15,35 +15,40 @@ class SearchBasicFragment: BaseFragment<FragmentSearchBasicBinding>(FragmentSear
 
     override fun initAfterBinding() {
         initAdapter()
-
-        searchRecentArray.apply {
-            add(DeliverySearchRecent("햄버거"))
-            add(DeliverySearchRecent("양식"))
-            add(DeliverySearchRecent("피자"))
-            add(DeliverySearchRecent("야식"))
-            add(DeliverySearchRecent("한식"))
-            add(DeliverySearchRecent("족발"))
-            add(DeliverySearchRecent("치킨"))
-        }
-
-        searchRankArray.apply {
-            add(DeliveryRank(1, "1위"))
-            add(DeliveryRank(2, "2위"))
-            add(DeliveryRank(3, "3위"))
-            add(DeliveryRank(4, "4위"))
-            add(DeliveryRank(5, "5위"))
-            add(DeliveryRank(6, "6위"))
-            add(DeliveryRank(7, "7위"))
-            add(DeliveryRank(8, "8위"))
-            add(DeliveryRank(9, "9위"))
-            add(DeliveryRank(10, "10위"))
-        }
     }
 
     private fun initAdapter() {
+        // View가 생성될 때마다 array에 추가되는 것을 막기 위함!
+        if (searchRecentArray.size == 0) {
+            searchRecentArray.apply {
+                add(DeliverySearchRecent("햄버거"))
+                add(DeliverySearchRecent("양식"))
+                add(DeliverySearchRecent("피자"))
+                add(DeliverySearchRecent("야식"))
+                add(DeliverySearchRecent("한식"))
+                add(DeliverySearchRecent("족발"))
+                add(DeliverySearchRecent("치킨"))
+            }
+        }
+
         searchRecentAdapter = SearchRecentRVAdapter(searchRecentArray)
         binding.searchBasicRecentItemRv.adapter = searchRecentAdapter
         binding.searchBasicRecentItemRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        if (searchRankArray.size != 10) {
+            searchRankArray.apply {
+                add(DeliveryRank(1, "1위"))
+                add(DeliveryRank(2, "2위"))
+                add(DeliveryRank(3, "3위"))
+                add(DeliveryRank(4, "4위"))
+                add(DeliveryRank(5, "5위"))
+                add(DeliveryRank(6, "6위"))
+                add(DeliveryRank(7, "7위"))
+                add(DeliveryRank(8, "8위"))
+                add(DeliveryRank(9, "9위"))
+                add(DeliveryRank(10, "10위"))
+            }
+        }
 
         searchRankAdapter = SearchRankRVAdapter(searchRankArray)
         binding.searchBasicRankRv.adapter = searchRankAdapter
