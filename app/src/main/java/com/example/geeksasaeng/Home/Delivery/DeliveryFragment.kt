baf222a7b6650cc00b05c6ad91cc36368ca67bea
lee-june-young <uhiccup@gmail.com>
2022-07-30 00:06:56 +0900

    Fix: deliveryFragment Timer sec없앰

--- app/src/main/java/com/example/geeksasaeng/Home/Delivery/DeliveryFragment.kt
+++ app/src/main/java/com/example/geeksasaeng/Home/Delivery/DeliveryFragment.kt
@@ -103,7 +103,6 @@ class DeliveryFragment: BaseFragment<FragmentDeliveryBinding>(FragmentDeliveryBi
         var orderDay = Integer.parseInt(orderTime.substring(8, 10))
         var orderHours = Integer.parseInt(orderTime.substring(11, 13))
         var orderMinutes = Integer.parseInt(orderTime.substring(14, 16))
-        var orderSec = 0
 
         var currentTime = calculateToday()
         var todayYear = Integer.parseInt(currentTime.substring(0, 4))
@@ -112,13 +111,12 @@ class DeliveryFragment: BaseFragment<FragmentDeliveryBinding>(FragmentDeliveryBi
         var todayHours = Integer.parseInt(currentTime.substring(11, 13))
         Log.d("calculateTime-HOUR", todayHours.toString())
         var todayMinutes = Integer.parseInt(currentTime.substring(14, 16))
-        var todaySec = Integer.parseInt(currentTime.substring(17, 19))
 
         var today = Calendar.getInstance().apply {
             set(Calendar.YEAR, todayYear)
             set(Calendar.MONTH, todayMonth)
             set(Calendar.DAY_OF_MONTH, todayDay)
-        }.timeInMillis + (60000 * 60 * todayHours) + (60000 * todayMinutes)+ (1000* todaySec)
+        }.timeInMillis + (60000 * 60 * todayHours) + (60000 * todayMinutes)
 
         var order = Calendar.getInstance().apply {
             set(Calendar.YEAR, orderYear)
