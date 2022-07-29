74194978d27adf785067e3b78e4adb0d167f5399
lee-june-young <uhiccup@gmail.com>
2022-07-30 01:21:49 +0900

    Fix: 파티보기 수령장소 물흐르는 애니메이션

--- app/src/main/java/com/example/geeksasaeng/Home/Party/LookPartyFragment.kt
+++ app/src/main/java/com/example/geeksasaeng/Home/Party/LookPartyFragment.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.os.Handler
 import android.text.method.Touch
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
@@ -39,6 +41,7 @@ class LookPartyFragment: BaseFragment<FragmentLookPartyBinding>(FragmentLookPart
     override fun initAfterBinding() {
         initClickListener()
         binding.lookPartyProgressBar.visibility = View.VISIBLE
+        binding.lookLocateText.isSelected = true // 물흐르는 애니메이션
 
         // 파티 수정하기, 신고하기 Stack에서 제거
         (context as MainActivity).supportFragmentManager.popBackStack("partyUpdate", FragmentManager.POP_BACK_STACK_INCLUSIVE)
@@ -51,8 +54,6 @@ class LookPartyFragment: BaseFragment<FragmentLookPartyBinding>(FragmentLookPart
         handler.postDelayed({
             initDeliveryPartyData()
         }, 200)
-
-        binding.lookCategoryText.isSelected = true
     }
 
     override fun onStop() {
