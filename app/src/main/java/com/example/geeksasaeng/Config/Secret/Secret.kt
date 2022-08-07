e696b313d3699cc5203d10748897de03d537a7d3
Neo <gamebox27@naver.com>
2022-08-08 08:00:46 +0900

    [FEAT] Secret key git Ignore 적용

---
+++ app/src/main/java/com/example/geeksasaeng/Config/Secret/Secret.kt
@@ -0,0 +1,8 @@
+package com.example.geeksasaeng.Config.Secret
+
+object Secret {
+    // 네이버 KEY
+    var OAUTH_CLIENT_ID: String = "Kvyd6swFfWHQJgjWaHr1"
+    var OAUTH_CLIENT_SECRET = "pq8VgJC5sn"
+    var OAUTH_CLIENT_NAME = "긱사생"
+}
