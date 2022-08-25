f3a13dfbac2a3bc5615d7be75e47184cae63c1b9
lee-june-young <uhiccup@gmail.com>
2022-08-26 00:13:28 +0900

    Style: 이용약관 linespacing주고, profile 디자인 수정중

--- app/src/main/java/com/example/geeksasaeng/Home/Party/ReportParty/PartyReportFragment.kt
+++ app/src/main/java/com/example/geeksasaeng/Home/Party/ReportParty/PartyReportFragment.kt
@@ -28,38 +28,26 @@ class PartyReportFragment: BaseFragment<FragmentDeliveryPartyReportBinding>(Frag
         reportedMemberId = requireArguments().getInt("reportedMemberId")
 
         // 파티 게시글 신고
-        binding.reportPartyPost1.setOnClickListener { changeActivity(0, binding.reportPartyPost1Tv.text.toString(), 1) }
-        binding.reportPartyPost2.setOnClickListener { changeActivity(0, binding.reportPartyPost2Tv.text.toString(), 2) }
-        binding.reportPartyPost3.setOnClickListener { changeActivity(0, binding.reportPartyPost3Tv.text.toString(), 3) }
-        binding.reportPartyPost4.setOnClickListener { changeActivity(1, binding.reportPartyPost4Tv.text.toString(), 4) }
+        binding.reportPartyPost1.setOnClickListener { changeActivity( binding.reportPartyPost1Tv.text.toString(), 1) }
+        binding.reportPartyPost2.setOnClickListener { changeActivity( binding.reportPartyPost2Tv.text.toString(), 2) }
+        binding.reportPartyPost3.setOnClickListener { changeActivity( binding.reportPartyPost3Tv.text.toString(), 3) }
+        binding.reportPartyPost4.setOnClickListener { changeActivity( binding.reportPartyPost4Tv.text.toString(), 4) }
 
         // 사용자 신고
-        binding.reportPartyUser1.setOnClickListener { changeActivity(0, binding.reportPartyUser1Tv.text.toString(), 5) }
-        binding.reportPartyUser2.setOnClickListener { changeActivity(0, binding.reportPartyUser2Tv.text.toString(), 6) }
-        binding.reportPartyUser3.setOnClickListener { changeActivity(0, binding.reportPartyUser3Tv.text.toString(), 7) }
-        binding.reportPartyUser4.setOnClickListener { changeActivity(0, binding.reportPartyUser4Tv.text.toString(), 8) }
-        binding.reportPartyUser5.setOnClickListener { changeActivity(0, binding.reportPartyUser5Tv.text.toString(), 9) }
-        binding.reportPartyUser6.setOnClickListener { changeActivity(1, binding.reportPartyUser6Tv.text.toString(), 10) }
+        binding.reportPartyUser1.setOnClickListener { changeActivity( binding.reportPartyUser1Tv.text.toString(), 5) }
+        binding.reportPartyUser2.setOnClickListener { changeActivity( binding.reportPartyUser2Tv.text.toString(), 6) }
+        binding.reportPartyUser3.setOnClickListener { changeActivity( binding.reportPartyUser3Tv.text.toString(), 7) }
+        binding.reportPartyUser4.setOnClickListener { changeActivity( binding.reportPartyUser4Tv.text.toString(), 8) }
+        binding.reportPartyUser5.setOnClickListener { changeActivity( binding.reportPartyUser5Tv.text.toString(), 9) }
+        binding.reportPartyUser6.setOnClickListener { changeActivity( binding.reportPartyUser6Tv.text.toString(), 10) }
     }
 
-    private fun changeActivity(option: Int, reportName: String, reportCategoryId: Int) {
-        when(option) {
-            0 -> {
-                val intent = Intent(activity, PartyReportOptionActivity::class.java)
-                intent.putExtra("reportName", reportName)
-                intent.putExtra("reportCategoryId", reportCategoryId)
-                intent.putExtra("reportedDeliveryPartyId", reportedDeliveryPartyId)
-                intent.putExtra("reportedMemberId", reportedMemberId)
-                startActivity(intent)
-            }
-            1 -> {
-                val intent = Intent(activity, PartyReportNoOptionActivity::class.java)
-                intent.putExtra("reportName", reportName)
-                intent.putExtra("reportCategoryId", reportCategoryId)
-                intent.putExtra("reportedDeliveryPartyId", reportedDeliveryPartyId)
-                intent.putExtra("reportedMemberId", reportedMemberId)
-                startActivity(intent)
-            }
-        }
+    private fun changeActivity(reportName: String, reportCategoryId: Int) {
+        val intent = Intent(activity, PartyReportOptionActivity::class.java)
+        intent.putExtra("reportName", reportName)
+        intent.putExtra("reportCategoryId", reportCategoryId)
+        intent.putExtra("reportedDeliveryPartyId", reportedDeliveryPartyId)
+        intent.putExtra("reportedMemberId", reportedMemberId)
+        startActivity(intent)
     }
 }
