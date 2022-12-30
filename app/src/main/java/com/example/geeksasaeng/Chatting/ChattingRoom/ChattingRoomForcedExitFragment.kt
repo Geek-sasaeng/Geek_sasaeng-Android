b62f3a56b3ae01bbb3c4d3f9e597d77e1e7e0138
lee-june-young <uhiccup@gmail.com>
2022-12-31 05:34:55 +0900

    Feats: 강제퇴장 뷰 생성
    - 강제퇴장 fragment = ChattingRoomForcedExitFragment
    - 강제퇴장 dialog = DialogForcedExit

---
+++ app/src/main/java/com/example/geeksasaeng/Chatting/ChattingRoom/ChattingRoomForcedExitFragment.kt
@@ -0,0 +1,11 @@
+package com.example.geeksasaeng.Chatting.ChattingRoom
+
+import com.example.geeksasaeng.Utils.BaseFragment
+import com.example.geeksasaeng.databinding.FragmentChattingRoomForcedExitBinding
+
+class ChattingRoomForcedExitFragment : BaseFragment<FragmentChattingRoomForcedExitBinding>(FragmentChattingRoomForcedExitBinding::inflate) {
+
+    override fun initAfterBinding() {
+
+    }
+}
