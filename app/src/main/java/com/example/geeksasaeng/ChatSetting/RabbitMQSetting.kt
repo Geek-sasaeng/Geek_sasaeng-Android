12e10baad9cc960a9b0c730229f5dec30ae5646a
JM2308 <wlals2987@naver.com>
2023-01-05 18:32:07 +0900

    Refactor: Change JDK Version for RabbitMQ

---
+++ app/src/main/java/com/example/geeksasaeng/ChatSetting/RabbitMQSetting.kt
@@ -0,0 +1,9 @@
+package com.example.geeksasaeng.ChatSetting
+
+class RabbitMQSetting {
+    private var queue_name = "test"
+
+    fun main() {
+        // val factory = ConnectionFactory()
+    }
+}
