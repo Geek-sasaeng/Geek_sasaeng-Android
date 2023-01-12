a8bd3369ea4125219bb827ff6d63a63d95a077c5
lee-june-young <uhiccup@gmail.com>
2023-01-13 03:30:01 +0900

    Feats: 내 정보 상세 조회 다이얼로그 생성
    - GEEKSA-123

--- app/src/main/java/com/example/geeksasaeng/Profile/DialogProfileDetail.kt
+++ app/src/main/java/com/example/geeksasaeng/Profile/DialogProfileDetail.kt
@@ -1,4 +1,36 @@
 package com.example.geeksasaeng.Profile
 
-class DialogProfileDetail {
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import com.example.geeksasaeng.databinding.DialogProfileDetailLayoutBinding
+
+class DialogProfileDetail : DialogFragment()  {
+
+    lateinit var binding: DialogProfileDetailLayoutBinding
+
+    override fun onResume() {
+        super.onResume()
+        val params = dialog!!.window!!.attributes
+        params.width = WindowManager.LayoutParams.WRAP_CONTENT
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT
+        dialog!!.window!!.attributes = params as WindowManager.LayoutParams
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DialogProfileDetailLayoutBinding.inflate(inflater, container, false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // 배경 투명하게 만들어줘야 둥근 테두리가 보인다.
+        return binding.root
+    }
+    
 }
