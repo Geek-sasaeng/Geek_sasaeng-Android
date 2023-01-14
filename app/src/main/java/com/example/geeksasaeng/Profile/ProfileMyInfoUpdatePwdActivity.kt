d330cdce6f9c12df7076737179b426a5eb9aaaa5
lee-june-young <uhiccup@gmail.com>
2023-01-14 16:22:14 +0900

    Style: 비밀번호 변경 View
    - Related to GEEKSA-135

---
+++ app/src/main/java/com/example/geeksasaeng/Profile/ProfileMyInfoUpdatePwdActivity.kt
@@ -0,0 +1,10 @@
+package com.example.geeksasaeng.Profile
+
+import com.example.geeksasaeng.Utils.BaseActivity
+import com.example.geeksasaeng.databinding.ActivityProfileMyInfoUpdatePasswordBinding
+
+class ProfileMyInfoUpdatePwdActivity: BaseActivity<ActivityProfileMyInfoUpdatePasswordBinding>(ActivityProfileMyInfoUpdatePasswordBinding::inflate) {
+    override fun initAfterBinding() {
+
+    }
+}
