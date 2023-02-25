224c9b43ef0a41e893a1872dc3728e1abdd1b7d0
lee-june-young <uhiccup@gmail.com>
2023-02-25 22:59:33 +0900

    Feats: 사용자 단계안내-> 배달파티하러가기 기능 구현 + 카카오 문의하기 뒤 logo 해결

--- app/src/main/java/com/example/geeksasaeng/Profile/ProfileCard/ProfileLevelIntroductionActivity.kt
+++ app/src/main/java/com/example/geeksasaeng/Profile/ProfileCard/ProfileLevelIntroductionActivity.kt
@@ -1,8 +1,10 @@
 package com.example.geeksasaeng.Profile.ProfileCard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil.setContentView
+import com.example.geeksasaeng.MainActivity
 import com.example.geeksasaeng.R
 import com.example.geeksasaeng.Utils.BaseActivity
 import com.example.geeksasaeng.databinding.ActivityProfileLevelIntroductionBinding
@@ -15,6 +17,14 @@ class ProfileLevelIntroductionActivity : BaseActivity<ActivityProfileLevelIntrod
     }
 
     private fun initClickListener() {
+
+        binding.profileLevelIntroductionGoMainBtn.setOnClickListener {
+            //홈 화면으로 이동하기
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
+
         binding.profileLevelIntroductionBackBtn.setOnClickListener {
             finish()
         }
