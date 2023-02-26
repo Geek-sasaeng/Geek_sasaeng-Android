0abcd96c7c5dd6695d82248c1dcc87b4ac441e84
lee-june-young <uhiccup@gmail.com>
2023-02-26 11:36:36 +0900

    Fix: 프로필 고객센터 TOS2 수정

--- app/src/main/java/com/example/geeksasaeng/Signup/Tos1Activity.kt
+++ app/src/main/java/com/example/geeksasaeng/Signup/Tos1Activity.kt
@@ -1,6 +1,7 @@
 package com.example.geeksasaeng.Signup
 
 import android.util.Log
+import android.view.View
 import com.example.geeksasaeng.Signup.Basic.StepFourFragment.Companion.serviceTemrsAgree
 import com.example.geeksasaeng.Signup.Naver.StepNaverTwoFragment.Companion.socialServiceTemrsAgree
 import com.example.geeksasaeng.Utils.BaseActivity
@@ -10,11 +11,19 @@ import com.example.geeksasaeng.databinding.ActivityTos1Binding
 class Tos1Activity : BaseActivity<ActivityTos1Binding>(ActivityTos1Binding::inflate){
 
     private var isSocial : Boolean = false // 네이버 회원가입 유무
+    var status: String? = null
 
     override fun initAfterBinding() {
-        isSocial = intent.getBooleanExtra("isSocial", false)
+        status = intent.getStringExtra("status")
+
+        if(status!="signUp"){ // 내정보뷰에서 보는 경우가 있다.
+            binding.tos1BottomBar.visibility = View.GONE
+        }else{ //회원가입창에서 들어온 경우
+            isSocial = intent.getBooleanExtra("isSocial", false)
+        }
+
         Log.d("cherry", "social"+intent.getBooleanExtra("isSocial", false).toString())
-        initClickListener()
+        initClickListener() 
     }
 
     private fun initClickListener(){
